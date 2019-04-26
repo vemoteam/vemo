fc2f5a79fd8e8a59e0d510970d5fcb26f3ee192f
dongyuanxin <yuanxin.me@gmail.com>
2019-04-26 15:20:24 +0800

    add local server for test

---
+++ test/server/vemofile.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const router = require('koa-joi-router')
+const Joi = router.Joi
+
+module.exports = {
+    'host': 'localhost',
+    'port': 5001,
+    'root': path.resolve('.'),
+    'routes': [
+        {
+            path: 'routes/api.js',
+            route: '/api',
+            method: 'post',
+            validate: {
+                type: 'json',
+                continueOnError: false,
+                body: {
+                    param1: Joi.string(),
+                    param2: Joi.number()
+                }
+            }
+        },
+        {
+            path: 'routes/index.js',
+            template: 'templates/index.html',
+            route: '/home',
+            method: 'get'
+        },
+        {
+            type: 'static',
+            path: 'public'
+        }
+    ]
+}
+
