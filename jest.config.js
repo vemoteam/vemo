34ed29bf4ef48f8e4849ad717a9cc7846f4680c8
dongyuanxin <yuanxin.me@gmail.com>
2019-05-05 14:04:00 +0800

    hotfix puppeteer timeout error

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
         '<rootDir>/test/', 
         '<rootDir>/node_modules/'
     ],
+    setupFilesAfterEnv: [
+        '<rootDir>/jest.setup.js'
+    ],
     collectCoverage: true,
     testEnvironment: 'node'
 }
