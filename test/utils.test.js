8cfc8f02c88f7ebe7659decd11441d4cc02473c0
dongyuanxin <yuanxin.me@gmail.com>
2019-06-01 12:28:31 +0800

    change: up test coverage rate

--- test/utils.test.js
+++ test/utils.test.js
@@ -8,6 +8,7 @@ describe('utils.js', () => {
     test('logger', () => {
         const { logger } = utils
         const levels = ['log', 'info', 'success', 'warn', 'error']
+        levels.forEach((level) => logger[level]('just a test'))
         const valid = levels.reduce((pre, current) => pre && typeof logger[current] === 'function', true)
         expect(valid).toBeTruthy()
     })
